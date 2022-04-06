7a575bb8e59426d78267287197b0fc300dd49d12
Eriko <erikoa.93@gmail.com>
2022-04-06 17:39:03 -0300

    Created and iniciated lession about heritage

---
+++ Lessions/Heritage/Heritage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Heritage.Entities;
+
+namespace Heritage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Eriko", 100, 500);
+
+            Console.WriteLine(account.Balance);
+            // account.Balance = 200; --> Não é possível devido o properties está com o set com a proteção "protected".
+        }
+    }
+}
