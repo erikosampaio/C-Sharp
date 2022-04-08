8381c81b8dac827e8501b9e7e4ecd8f466e3ee03
Eriko Sampaio <erikoa.93@gmail.com>
2022-04-08 18:55:27 -0300

    Ok

---
+++ Works/Taxpayer/Taxpayer/Entities/Company.cs
@@ -0,0 +1,26 @@
+﻿namespace Taxpayer.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployee { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(string name, double anualIncome, int numberOfEmployee)
+            : base(name, anualIncome)
+        {
+            NumberOfEmployee = numberOfEmployee;
+        }
+
+        public override double TaxesPaid()
+        {
+            if (NumberOfEmployee > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            return AnualIncome * 0.16;
+        }
+    }
+}
---
+++ Works/Taxpayer/Taxpayer/Entities/Individual.cs
@@ -0,0 +1,43 @@
+﻿namespace Taxpayer.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual()
+        {
+        }
+
+        public Individual(string name, double anualIncome, double healthExpenditures)
+            : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double TaxesPaid()
+        {
+            if (AnualIncome < 20000.0)
+            {
+                if (HealthExpenditures > 0)
+                {
+                    double taxesPaid = AnualIncome * 0.15;
+                    return taxesPaid - HealthExpenditures * 0.5;
+                }
+                else
+                {
+                    double taxesPaid = AnualIncome * 0.15;
+                    return taxesPaid;
+                }
+            }
+            else if (HealthExpenditures > 0)
+            {
+                double taxesPaid = AnualIncome * 0.25;
+                return taxesPaid - HealthExpenditures * 0.5;
+            }
+            else
+            {
+                return AnualIncome * 0.25;
+            }
+        }
+    }
+}
