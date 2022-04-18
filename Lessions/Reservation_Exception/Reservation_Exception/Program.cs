c6e494bcbe0e47e0266b130322047755fca8ed94
Eriko Sampaio <erikoa.93@gmail.com>
2022-04-18 17:49:01 -0300

    New works about Exceptions

---
+++ Lessions/Reservation_Exception/Reservation_Exception/Program.cs
@@ -0,0 +1,49 @@
+﻿using Reservation_Exception.Entities;
+using Reservation_Exception.Entities.Exceptions;
+using System;
+
+namespace Reservation_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.Write("Room number: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(room, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Works/Account_Exception/Account_Exception/Entities/Account.cs
@@ -0,0 +1,54 @@
+﻿using Account_Exception.Entities.Exceptions;
+
+namespace Account_Exception.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            if (balance < 0 || withdrawLimit < 0)
+            {
+                throw new DomainException("Error: Invalid value(s)!");
+            }
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new DomainException("Error deposit: Invalid amount");
+            }
+            else
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
